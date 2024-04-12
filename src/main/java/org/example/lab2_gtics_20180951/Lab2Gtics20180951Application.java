e9d44f6925d7727fdf1e3d245a757f121cac2f67
unknown <eduardo.carbajal@pucp.edu.pe>
2024-04-11 20:25:50 -0500

    primer commit

---
+++ src/main/java/org/example/lab2_gtics_20180951/Lab2Gtics20180951Application.java
@@ -0,0 +1,13 @@
+package org.example.lab2_gtics_20180951;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2Gtics20180951Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2Gtics20180951Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/lab2_gtics_20180951/Lab2Gtics20180951ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab2_gtics_20180951;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2Gtics20180951ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
